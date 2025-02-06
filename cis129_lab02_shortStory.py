2515f239e7c05e89fb5edae0db8974296f7d18bc
Matt-OS117 <mfitzsimmons4@mail.pima.edu>
2025-02-06 15:48:38 -0700

    Update cis129_lab02_shortStory.py

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,11 @@
-print('Hello, World')
+#Matt F.
+#First Python based lab in CIS129
+#It prints a short story, inspired by a true story.
+#My mom asked me one day if it was possible to download more RAM.
+#It was too late.
+
+print('I am a computer, Hello, World')
+print('An old lady bought me at the Genius Bar')
+print('She took me home and downloaded 598GB worth of RAM')
+print('My circuitry did not like this flavor of RAM, and I feel ill')
+print('I think I'm going to, I see a lig01101000 01110100....')
